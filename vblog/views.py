a0f94c3b86b9a13c77494646920f7645ee52b177
Guru Govindan <gxguru@gmail.com>
2016-06-20 12:55:01 -0700

    Changes to increase the width of the page content and increase pagination from 2 to 10

--- vblog/views.py
+++ vblog/views.py
@@ -5,7 +5,7 @@ from . import models
 class VBlogIndex(generic.ListView):
     queryset = models.Entry.objects.published()
     template_name = "home.html"
-    paginate_by = 2
+    paginate_by = 10
 
 class VBlogDetail(generic.DetailView):
     model = models.Entry
