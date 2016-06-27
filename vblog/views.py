57e9349e321af429bf922328dbc9c9da79ab8fbc
ggovindan <gxguru@gmail.com>
2016-06-27 13:46:04 -0700

    customizing for the group. Adding favicon changing content etc

--- vblog/views.py
+++ vblog/views.py
@@ -8,7 +8,7 @@ class VBlogIndex(generic.ListView):
     queryset = models.Entry.objects.published()
     template_name = "home.html"
     context_object_name = "entry_list" # This will be the value used in the template to refer to the list
-    paginate_by = 2
+    paginate_by = 4
 
 class VBlogDetail(generic.DetailView):
     model = models.Entry
