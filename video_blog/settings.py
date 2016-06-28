b136aa9a5e42248b30991fbed441f1a15b236c74
gugovind <gugovind@cisco.com>
2016-06-28 00:09:13 -0700

    commented out some sqlite3 stuff

--- video_blog/settings.py
+++ video_blog/settings.py
@@ -105,12 +105,12 @@ MEDIA_URL = '/static/images/'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 # Internationalization
