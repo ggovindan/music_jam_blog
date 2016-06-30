eae66ba8bbfa42e5883a6e77a9bc61c5ec7a41ff
ggovindan <gxguru@gmail.com>
2016-06-29 19:03:02 -0700

    Changing database name for snncisco project

--- video_blog/settings.py
+++ video_blog/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATABASES = {
     'default': {
                 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-                'NAME': 'jamatravi',
+                'NAME': 'snncisco',
                 'USER': 'gugovind',
                }
 }
